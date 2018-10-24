f66035c713068ddfcb85d49de9e1b411edef2032
Ruben Verborgh <ruben@verborgh.org>
2018-10-24 22:29:45 +0200

    Don't include dist files in repository.

--- dist/types/components/login-component/login-component.d.ts
+++
@@ -1,9 +0,0 @@
-import '../../stencil.core';
-import 'solidAuth';
-export declare class LoginComponent {
-    loginProvider: string;
-    callback: string;
-    providerChangedHandler(event: CustomEvent): void;
-    login(): Promise<void>;
-    render(): JSX.Element;
-}
